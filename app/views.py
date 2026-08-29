471aaea0c9ff589c5bcb2e40f101ae9a4d707a6f
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 20:21:17 -0300

    feat: adicionando função para buscar as localizações contidas na api

--- app/views.py
+++ app/views.py
@@ -26,4 +26,28 @@ def buscarPersoViews(request):
         'erro': erro,
         'nome': nome,
     })
+def buscarLocalizacaoViews(request):
+    nome = (request.GET.get('nome'))
+
+    info = None
+    erro = None
+
+    if (nome):
+        try:
+            resposta = requests.get(f"https://rickandmortyapi.com/api/location", params={"name": nome})
+
+            if resposta.status_code == 200:
+                info = resposta.json().get("results", [])
+            else:
+                erro = "Nenhum personagem encontrado com esse nome."
+
+        except requests.exceptions.RequestException:
+            erro = "Não foi possível acessar a API externa."
+
+    return render(request, 'perso.html', {
+        'info': info,
+        'erro': erro,
+        'nome': nome,
+    })
+
 
