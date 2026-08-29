6ba1d9c6aeb4038f9e5c8ad60158753655133dd7
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 20:31:47 -0300

    feat: adicionando função de busca de episodio

--- app/views.py
+++ app/views.py
@@ -39,15 +39,38 @@ def buscarLocalizacaoViews(request):
             if resposta.status_code == 200:
                 info = resposta.json().get("results", [])
             else:
-                erro = "Nenhum personagem encontrado com esse nome."
+                erro = "Nenhuma localização encontrada com esse nome."
 
         except requests.exceptions.RequestException:
             erro = "Não foi possível acessar a API externa."
 
-    return render(request, 'perso.html', {
+    return render(request, 'localizacao.html', {
         'info': info,
         'erro': erro,
         'nome': nome,
     })
 
+def buscarEpisodioViews(request):
+    nome = (request.GET.get('nome'))
+
+    info = None
+    erro = None
+
+    if (nome):
+        try:
+            resposta = requests.get(f"https://rickandmortyapi.com/api/episode", params={"name": nome})
+
+            if resposta.status_code == 200:
+                info = resposta.json().get("results", [])
+            else:
+                erro = "Nenhum episodio encontrado com esse nome."
+
+        except requests.exceptions.RequestException:
+            erro = "Não foi possível acessar a API externa."
+
+    return render(request, 'episodio.html', {
+        'info': info,
+        'erro': erro,
+        'nome': nome,
+    })
 
