fa26192d6c47f719b24a6dad170ff99de5bba7ad
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 16:52:19 -0300

    feat: Importando o EpisodioSerializers do serializers e criando a funcionalidade de fazer a busca de episodios.

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,5 +1,5 @@
 import requests
-from .serializers import PersonagemSerializers, LocalizacaoSerializers
+from .serializers import PersonagemSerializers, LocalizacaoSerializers, EpisodioSerializers
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -53,6 +53,41 @@ class LocalizacaoViewsets(viewsets.ViewSet):
             dados = resposta.json()
 
             serializer = LocalizacaoSerializers(
+                data=dados["results"],
+                many=True
+            )
+            serializer.is_valid(
+                raise_exception=True
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
+        except requests.exceptions.RequestException:
+
+            return Response(
+                {
+                    "erro": "Não foi possível acessar a API externa."
+                },
+                status=status.HTTP_503_SERVICE_UNAVAILABLE
+            )
+
+class EpisodioViewsets(viewsets.ViewSet):
+    def list(self, request):
+        try:
+            resposta = requests.get(
+                "https://rickandmortyapi.com/api/episode"
+            )
+            if resposta.status_code != 200:
+                return Response({
+                    "erro": "Erro ao consultar API externa."
+                },
+                status=status.HTTP_502_BAD_GATEWAY
+                )
+            dados = resposta.json()
+
+            serializer = EpisodioSerializers(
                 data=dados["results"],
                 many=True
             )
