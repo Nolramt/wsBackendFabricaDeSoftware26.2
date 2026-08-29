574e0b210d38e56e09ee1b1d89dde61037302a75
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 16:40:08 -0300

    feat: criando funcionalidade para mostrar as localizações/dimensões.

--- app/api/viewsets.py
+++ app/api/viewsets.py
@@ -1,5 +1,5 @@
 import requests
-from .serializers import PersonagemSerializers
+from .serializers import PersonagemSerializers, LocalizacaoSerializers
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
@@ -30,6 +30,40 @@ class PersonagemViewsets(viewsets.ViewSet):
                 serializer.data,
                 status=status.HTTP_200_OK
             )
+        except requests.exceptions.RequestException:
+
+            return Response(
+                {
+                    "erro": "Não foi possível acessar a API externa."
+                },
+                status=status.HTTP_503_SERVICE_UNAVAILABLE
+            )
+class LocalizacaoViewsets(viewsets.ViewSet):
+    def list(self, request):
+        try:
+            resposta = requests.get(
+                "https://rickandmortyapi.com/api/location"
+            )
+            if resposta.status_code != 200:
+                return Response({
+                    "erro": "Erro ao consultar API externa."
+                },
+                status=status.HTTP_502_BAD_GATEWAY
+                )
+            dados = resposta.json()
+
+            serializer = LocalizacaoSerializers(
+                data=dados["results"],
+                many=True
+            )
+            serializer.is_valid(
+                raise_exception=True
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+
         except requests.exceptions.RequestException:
 
             return Response(
