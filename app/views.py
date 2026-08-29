9ccc183989a952dacbd0f6074d28b4d4134b3056
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 20:15:34 -0300

    feat: criando função para pesquisar personagens especificos pelo nome

--- app/views.py
+++ app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def buscarPersoViews(request):
+    nome = (request.GET.get('nome'))
+
+    info = None
+    erro = None
+
+    if (nome):
+        try:
+            resposta = requests.get(f"https://rickandmortyapi.com/api/character", params={"name": nome})
+
+            if resposta.status_code == 200:
+                info = resposta.json().get("results", [])
+            else:
+                erro = "Nenhum personagem encontrado com esse nome."
+
+        except requests.exceptions.RequestException:
+            erro = "Não foi possível acessar a API externa."
+
+    return render(request, 'perso.html', {
+        'info': info,
+        'erro': erro,
+        'nome': nome,
+    })
+
