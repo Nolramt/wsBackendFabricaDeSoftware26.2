a374bb52aa419b38d4f18ff2361a1d5232996d56
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 16:20:13 -0300

    feat: Criando a class PersonagemViewsets para fazer requisição na api de rick and morty juntamente com o tratamento de erro

---
+++ app/api/viewsets.py
@@ -0,0 +1,40 @@
+import requests
+from .serializers import PersonagemSerializers
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+
+class PersonagemViewsets(viewsets.ViewSet):
+    def list(self, request):
+        try:
+            resposta = requests.get(
+                "https://rickandmortyapi.com/api/character",
+            )
+            if resposta.status_code != 200:
+                return Response(
+                    {
+                        "erro": "Erro ao consultar API externa."
+                    },
+                    status=status.HTTP_502_BAD_GATEWAY
+                )
+            dados = resposta.json()
+
+            serializer = PersonagemSerializers(
+                data=dados["results"],
+                many=True
+            )
+            serializer.is_valid(
+                raise_exception=True
+            )
+            return Response(
+                serializer.data,
+                status=status.HTTP_200_OK
+            )
+        except requests.exceptions.RequestException:
+
+            return Response(
+                {
+                    "erro": "Não foi possível acessar a API externa."
+                },
+                status=status.HTTP_503_SERVICE_UNAVAILABLE
+            )
