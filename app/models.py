e37cc5081429065abdbbee05a16e60fc87112c44
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 22:33:23 -0300

    feat: Criando classe PastaFavoritos para relacionamento de chave estrangeira com as outras classes favoritas

--- app/models.py
+++ app/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class PastaFavoritos(models.Model):
+    nome = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nome
+    
 class PersonagemFav(models.Model):
     id_externo = models.IntegerField(unique=True)
     name = models.CharField(max_length=254)
@@ -10,7 +16,9 @@ class PersonagemFav(models.Model):
     type = models.CharField(max_length=100, blank=True)
     gender = models.CharField(max_length=50)
     image = models.URLField()
-    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+    pasta = models.ForeignKey(
+        PastaFavoritos, on_delete=models.SET_NULL, null=True, blank=True, related_name="personagens"
+    )
 
     def __str__(self):
         return self.name
@@ -19,7 +27,9 @@ class LocalizacaoFav(models.Model):
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=100)
     dimension = models.CharField(max_length=255)
-    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+    pasta = models.ForeignKey(
+        PastaFavoritos, on_delete=models.SET_NULL, null=True, blank=True, related_name="localizacoes"
+    )
 
     def __str__(self):
         return self.name
@@ -30,7 +40,9 @@ class EpisodioFav(models.Model):
     name = models.CharField(max_length=255)
     air_date = models.CharField(max_length=100)
     episode = models.CharField(max_length=50)
-    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+    pasta = models.ForeignKey(
+        PastaFavoritos, on_delete=models.SET_NULL, null=True, blank=True, related_name="episodios"
+    )
 
     def __str__(self):
         return self.name
