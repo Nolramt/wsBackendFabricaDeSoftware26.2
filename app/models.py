1a5245231fb29699dabc9a34dc77807929c3e887
Nolramt <marlonaraujodasilva2007@gmail.com>
2026-08-29 21:10:20 -0300

    feat: criando classes PersonagemFav, LocalizacaoFav e EpisodioFav para armazenar os respectivos que você gostar no banco interno

--- app/models.py
+++ app/models.py
@@ -2,3 +2,35 @@ from django.db import models
 
 # Create your models here.
 
+class PersonagemFav(models.Model):
+    id_externo = models.IntegerField(unique=True)
+    name = models.CharField(max_length=254)
+    status = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, blank=True)
+    gender = models.CharField(max_length=50)
+    image = models.URLField()
+    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+
+    def __str__(self):
+        return self.name
+class LocalizacaoFav(models.Model):
+    id_externo = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=100)
+    dimension = models.CharField(max_length=255)
+    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+
+    def __str__(self):
+        return self.name
+
+
+class EpisodioFav(models.Model):
+    id_externo = models.IntegerField(unique=True)
+    name = models.CharField(max_length=255)
+    air_date = models.CharField(max_length=100)
+    episode = models.CharField(max_length=50)
+    pasta = models.CharField(max_length=100, blank=True, default="Geral")
+
+    def __str__(self):
+        return self.name
